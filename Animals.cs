a8f81da93b2a098f487024e902684d02a16fab4b
LuanneZati <luanezattilima@hotmail.com>
2023-01-19 14:27:19 -0300

    first commit

---
+++ Animals.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace AnimalsControl
+{
+    class Animals
+    {
+        private string[] name = new string[5];
+        private string[] type = new string[5];
+        private int dog = 0;
+        private int cat = 0;
+        private int fish = 0;
+
+        public string[] Name
+        {
+            get { return name; }
+        }
+        public string[] Type
+        {
+            get { return type; }
+        }
+
+        public void Counter()
+        {
+            foreach (string type in type)
+            {
+                if (type.ToUpper() == "DOG")
+                {
+                    dog++;
+                }
+                else if (type.ToUpper() == "CAT")
+                {
+                    cat++;
+                }
+                else
+                {
+                    fish++;
+                }
+            }
+        }
+        public void show()
+        {
+            Console.WriteLine($"\nDogs: {dog} \nCats: {cat} \nFishes: {fish}");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace AnimalsControl
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Animals animal = new Animals();
+            Console.WriteLine("Enter 5 animals's name (Only dog, cat or fish)");
+            Console.WriteLine();
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.Write($"{i+1} Animal name: ");
+                animal.Name[i] = Console.ReadLine();
+                Console.Write("Type: ");
+                animal.Type[i] = Console.ReadLine();
+                Console.WriteLine();
+            }
+            animal.Counter();
+            animal.show();
+        }
+    }
+}
